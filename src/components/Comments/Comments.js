99ff55902996d00b121913c70dbf970ab2499966
Jonathan Portillo <jonathanportillo@MacBook-Pro.home>
2020-07-28 20:20:30 -0400

    looking for errors

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Comment from './Comment';
-import './Comments.css';
+import React from "react";
+import Comment from "./Comment";
+import "./Comments.css";
 
-const Comments = props => {
+const Comments = (props) => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+  console.log(props);
   return (
     <div>
+      {comments.comments.map((comment) => {
+        return <Comment comments={comment} />;
+      })}
       {/* map through the comments prop and render a Comment for every piece of data */}
     </div>
   );
